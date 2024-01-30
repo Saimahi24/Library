83a1e4e7a9fd30212718613132238a582c2d7291
Saimahi24 <96533587+Saimahi24@users.noreply.github.com>
2024-01-30 13:23:53 -0600

    Update lib.groovy

--- vars/lib.groovy
+++ vars/lib.groovy
@@ -1,6 +1,6 @@
 def gitdownload(repo)
 {
-  git 'https://github.com/Saimahi24/{repo}.git'
+  git 'https://github.com/Saimahi24/${repo}.git'
 }
 def gitbuild()
 {
@@ -8,7 +8,7 @@ def gitbuild()
 }
 def gitdeploy(jobname,ip,contextpath)
 {
-   sh 'scp /var/lib/jenkins/workspace/{jobname}/webapp/target/webapp.war ubuntu@{ip}:/var/lib/tomcat9/webapps/{contextpath}.war'
+   sh 'scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${contextpath}.war'
 }
 
 
