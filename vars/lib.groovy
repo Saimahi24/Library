fd8506e0931f8b32a94228622495e268bd58aac5
Saimahi24 <96533587+Saimahi24@users.noreply.github.com>
2024-01-30 13:25:47 -0600

    Update lib.groovy

--- vars/lib.groovy
+++ vars/lib.groovy
@@ -1,14 +1,14 @@
 def gitdownload(repo)
 {
-  git 'https://github.com/Saimahi24/${repo}.git'
+  git "https://github.com/Saimahi24/${repo}.git"
 }
 def gitbuild()
 {
-  sh 'mvn package'
+  sh "mvn package"
 }
 def gitdeploy(jobname,ip,contextpath)
 {
-   sh 'scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${contextpath}.war'
+   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${contextpath}.war"
 }
 
 
